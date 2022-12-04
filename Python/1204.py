10773ad8229b46183e7376b4b5b541257d3a6207
Zoeling23 <115608874+Zoeling23@users.noreply.github.com>
2022-12-04 11:55:08 +0800

    1204
    1204

---
+++ Python/1204.py
@@ -0,0 +1,13 @@
+print("hello")
+
+salary=877
+workhour=6
+savemoney=12888
+
+InputMoney=salary*workhour*15 #15天領一次錢
+HalfMonth=InputMoney-savemoney #半個月剩下可以花的錢
+print(HalfMonth*24) #一年可以花的錢
+
+"""
+多行註解
+"""
